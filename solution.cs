1ec2a78956230a46728c9634b7940caf6133830c
Kooreis <kooreis.official@gmail.com>
2023-08-03 02:29:45 -0500

    Added the 'mkdir' method to the FileSystem class. This method creates directories in the file system.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,7 @@
-class FileSystem {
-    constructor() {
-        this.root = {};
+mkdir(path) {
+        let node = this.root;
+        path.split('/').filter(Boolean).forEach(dir => {
+            if (!node[dir]) node[dir] = {};
+            node = node[dir];
+        });
     }
-}
